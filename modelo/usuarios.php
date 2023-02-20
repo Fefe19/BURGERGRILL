8d4459064892bb6005d5708c1f51f19f40855140
Fefe19 <119813509+Fefe19@users.noreply.github.com>
2023-02-20 19:53:05 +0100

    Add files via upload

--- modelo/usuarios.php
+++ modelo/usuarios.php
@@ -3,60 +3,59 @@
         public static function addUser($correo,$name,$psswd){
             $con = DataBase::connect();
 
-            
+            $hashedPassword = password_hash($psswd, PASSWORD_DEFAULT);
 
-            $stmt = $con->prepare("INSERT INTO usuario (correo,nombre,contraseña) VALUES (?,?,?)");
-            //Bind variables to the prepared statement as parameters
-            $stmt->bind_param("sss",$correo,$name,$psswd);
-            //Execute statement
+            $stmt = $con->prepare("SELECT correo FROM usuario WHERE correo = ?");
+            $stmt->bind_param("s", $correo);
             $stmt->execute();
+            $result = $stmt->get_result();
+        
+            if ($result->num_rows > 0) {
+                echo "<script>alert('Usuario registrado');</script>";
+            } else {
+                $stmt = $con->prepare("INSERT INTO usuario (correo, nombre, contraseña) VALUES (?, ?, ?)");
+                $stmt->bind_param("sss", $correo, $name, $hashedPassword);
+                $stmt->execute();
+            }
+        
             $con->close();
         }
 
-        public static function accesUser(){
+        public static function accesUser($mail, $password) {
             $con = DataBase::connect();
-            $mail = $_POST['correo'];	
-	        $password = $_POST['psswd'];
         
-            //$stmt = $con->prepare("SELECT correo, contraseña FROM usuario WHERE correo=? AND contraseña=? ");
-            //Bind variables to the prepared statement as parameters
-            //$stmt->bind_param("ss",$mail,$password);
-            //Execute statement
-            //$stmt->execute();
-
-            $sql = "SELECT id_usuario, correo, nombre, contraseña, rol FROM usuario WHERE correo = '$mail' AND contraseña='$password'";
-            
-            $result = mysqli_query($con, $sql);
-
+            $stmt = $con->prepare("SELECT id_usuario, correo, nombre, contraseña, rol FROM usuario WHERE correo = ?");
+            $stmt->bind_param("s", $mail);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        
             if (mysqli_num_rows($result) > 0) {
-
                 $row = mysqli_fetch_assoc($result);
-
-                
-
-                    $_SESSION['usuariosr'] = $row['id_usuario'];
-
-                    $_SESSION['correo'] = $row['correo'];
-
-                    $_SESSION['nombre'] = $row['nombre'];
-
-                    $_SESSION['contraseña'] = $row['contraseña'];
-
-                    $_SESSION['rol'] = $row['rol'];
-
-                    usuarioController::login();
-               
-            }
-
-        }
-
         
+                if (password_verify($password, $row['contraseña'])) {
+                    $_SESSION['usuariosr'] = $row['id_usuario'];
+                    $_SESSION['correo'] = $row['correo'];
+                    $_SESSION['nombre'] = $row['nombre'];
+                    $_SESSION['rol'] = $row['rol'];
+                    usuarioController::login();
+                } else {
+                    echo "<script>alert('Contraseña incorrecta');</script>";
+                }
+            } else {
+                echo "<script>alert('Correo incorrecto');</script>";
+            }
+        
+            $con->close();
+        }
 
         public static function modificarUsuario($alterCorreo, $alterNombre, $alterPsswd, $id_usuario){
             $con = DataBase::connect();
+
+            $hashedPassword = password_hash($alterPsswd, PASSWORD_DEFAULT);
+            
             $stmt = $con->prepare("UPDATE usuario SET correo= ?, nombre= ?, contraseña=? WHERE id_usuario=?");
             //Bind variables to the prepared statement as parameters
-            $stmt->bind_param("sssi", $alterCorreo, $alterNombre, $alterPsswd, $id_usuario);
+            $stmt->bind_param("sssi", $alterCorreo, $alterNombre, $hashedPassword, $id_usuario);
             //Execute statement
             $stmt->execute();
 
