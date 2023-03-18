1f397aef9ff0cdb25f3dafa64d48d8facfe6ad5b
Fefe19 <119813509+Fefe19@users.noreply.github.com>
2023-03-18 20:25:48 +0100

    Add files via upload

--- views/principal.php
+++ views/principal.php
@@ -7,6 +7,8 @@
         </div>
     </section>
 
+    
+
     <section class="container-fluid" style="background-color:black">
         <div class="row">
             <div class="col-lg-6 izquierda" style="background-color:#FFC03F">
@@ -20,6 +22,8 @@
             </div>
         </div>
     </section>
+
+    
 
     <section class="container-fluid" style="background-color:white" >
         <h2 class="mb-5 fs-1 text-center" style="margin-top: 50px;">¡NUESTRAS HAMBURGUESAS MÁS SOLICITADAS!</h2>
@@ -37,21 +41,21 @@
             </div>
         </div>
     </section>
-
+    
     <section class="container-fluid" style="background-color:#E0CA9F" >
         <h2 class="mb-5 fs-1 text-center pt-5" style="margin-top: 50px;">¡NUESTROS ENTRANTES MÁS SOLICITADAS!</h2>
         <div class="row justify-content-md-center seccion_ham">
             <div class="col-lg-4 mt-2">
                 <img src=<?=base_url."img/NACHOSH.webp"?> class="mx-auto" >
                 <h3 class="mx-auto letra_t my-2 fs-4">NACHOS DE CARNE Y CHILI</h3>
-                
+
             </div>
             <div class="col-lg-4 mt-2">
                 <img src=<?=base_url."img/BRAVAS.webp"?> class="mx-auto" >
                 <h3 class="mx-auto letra_t my-2 fs-4" >PATATAS BRAVAS</h3>
             </div>
             <div class="col-lg-6 mt-5 pb-5">
-            <a class="nav-link " href=<?=base_url.'/producto/muestraEntrantes'?>><button type="button" class="btn btn-outline-color1  fw-bold" style="height:50px; width:200px; border-color:black; border-width:3px; border-radius: 20px;">¡VER MÁS!</button></a>
+            <a class="nav-link " href=<?=base_url.'/producto/muestraEntrantes'?>><button type="button" class="btn btn-outline-color1 fw-bold" style="height:50px; width:200px; border-color:black; border-width:3px; border-radius: 20px;">¡VER MÁS!</button></a>
             </div>
         </div>
 
@@ -59,15 +63,13 @@
         </div>
     </section>
 
-    <section class="container-fluid" style="background-color:white" >
+    <section class="container-fluid mb-5" style="background-color:white" >
         <h2 class="mb-5 fs-1 text-center pt-5">¡NUESTRO RESTAURANTE!</h2>
-        <div class="row justify-content-md-center seccion_ham">
-            <div class="col-lg-6 mb-5 ">
-            <iframe  src="https://www.youtube.com/embed/S892mObU2jM" title="YouTube video player" id="video_movil" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+        <div class="justify-content-md-center seccion_ham position: relative;">
+            <button id="button" type="button" class=" btn btn-outline-color1 fw-bold" style=" z-index: 1; height:45px; margin-left:720px; margin-top:130px; width:140px; border-color:black; border-width:3px; border-radius: 20px; position: absolute;" ><a class="nav-link " href=<?=base_url.'/producto/carta'?>>VER CARTA</a></button>
+            <video  id="video" controls src=<?=base_url."video/video.mp4"?> width="420"  height="240"></video>
         </div>
-
-       
+        <!--<iframe  src="https://www.youtube.com/embed/S892mObU2jM" title="YouTube video player" id="video_movil" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->    
     </section>
 
-    
+<button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="clearModalShown()">Close</button>
